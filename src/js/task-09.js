2395bf103c4601a1829e26ea9151e2d7d01ef93a
Iryna-Tyshchuk <ukriris93@gmail.com>
2022-12-22 20:26:25 +0200

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorInfo = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
+function changeBodyBg(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorInfo.textContent = document.body.style.backgroundColor;
+}
+
+btnEl.addEventListener("click", changeBodyBg);
